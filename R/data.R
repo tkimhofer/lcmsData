8e03d0f737e9cbdf70bdc9a079f8a54472699f1a
Torben <tkimhofer@gmail.com>
2020-09-15 18:45:54 +0800

    und

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' UPLC-MS data for metabolic profiling
+#'
+#' A dataset containing raw UPLC-MS level 1 data.
+#' Data were acquired using hydrophilic interaction liquid chromatography (HILIC) coupled to electrospray (ESI) quadrupole - time of flight (Q-TOF) mass spectrometer equipped with a multi-channel plate detector. ESI was performed in positive ionisation mode.
+#'
+#' @format A data frame with 11.9 million rows and 4 variables:
+#' \describe{
+#'   \item{mz}{Mass to charge ratio}
+#'   \item{count}{Detector count}
+#'   \item{scanId}{Detector scan ID}
+#'   \item{scantime}{Scan time associated with detector scan ID}
+#' }
+#'
+"lcms"
